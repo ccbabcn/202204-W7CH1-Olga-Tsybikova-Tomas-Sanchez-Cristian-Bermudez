80de7516e237044bc41e86de6fc3ffe87e345262
Olga Tsybikova <tsy2701@gmail.com>
2022-05-18 22:37:56 +0200

    merged error files into one

--- server/middlewares/errors/errors.js
+++ server/middlewares/errors/errors.js
@@ -7,6 +7,16 @@ const notFoundError = (req, res) => {
   debug(chalk.red("Recieved a request for an unexisting endpoint"));
 };
 
+// eslint-disable-next-line no-unused-vars
+const generalError = (error, req, res, next) => {
+  debug(chalk.red(`Error: ${error.message}`));
+  const statusCode = error.statusCode ?? 500;
+  const errorMessage = error.errorMessage ? error.errorMessage : "Severe Error";
+
+  res.status(statusCode).json({ message: errorMessage });
+};
+
 module.exports = {
   notFoundError,
+  generalError,
 };
