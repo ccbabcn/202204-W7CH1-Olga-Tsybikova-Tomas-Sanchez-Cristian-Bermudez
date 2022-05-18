6469ef7e69dea9e936f51d79be907e0cd0fd2a81
Olga Tsybikova <tsy2701@gmail.com>
2022-05-19 00:07:09 +0200

    Tested 'notfound error'

--- server/middlewares/errors/errors.js
+++ server/middlewares/errors/errors.js
@@ -1,9 +1,8 @@
-require("dotenv").cofing();
 const chalk = require("chalk");
 const debug = require("debug")("series:middlewares:errors");
 
 const notFoundError = (req, res) => {
-  res.status(404).json({ msg: "This endpoint was not found" });
+  res.status(404).json({ message: "This endpoint was not found" });
   debug(chalk.red("Recieved a request for an unexisting endpoint"));
 };
 
